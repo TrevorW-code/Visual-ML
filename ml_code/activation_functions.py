2f9172acc087fdcea7c87da9f87a0c54fc632c5d
Trevor <trevorward001@gmail.com>
2024-02-07 21:54:14 -0700

    fix implementation

--- ml_code/activation_functions.py
+++ ml_code/activation_functions.py
@@ -1,11 +1,16 @@
+import math
 # activation functions in pure python
 
-# Leaky Relu
+### Leaky Relu ###
 def leaky_relu(x):
     return max(0.01*x,x)
 
-# Relu
+### Relu ###
 def relu(x):
     return max(0,x)
 
-# Softmax
+### Softplus ###
+# exponent to Eulers number
+def softplus(x):
+    return math.log(1 + 1/math.e**x) # need to check this tmr
+    # math.log(1 + math.exp(x))
